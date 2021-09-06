20643b7ff15bb366264685e08932dc88eb148d1e
prodnix <cssprod@gmail.com>
2021-09-06 22:14:47 +0200

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -3,3 +3,6 @@ welcome_messages = {"whatsupbitches": "Whats up bitches",
     "sprinkler": "My sprinkler goes like thisstststststststststststststststststststststststst and comes back like titttttttttttttttttttttttttttttttttttttte",
     "thedj": "The DJ has entered the room"
     }
+
+insult_adj = ["absolute", "utter", "incompetent", "hidious", "unbareable", "total", "massive", "useless"]
+insult_noun = ["cock nosher", "tool", "cretin", "hoop sniffer", "imbecile", "lemming"]
