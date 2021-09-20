62378d75d36be50471b4877775ebecd2fa69cc5b
prodnix <cssprod@gmail.com>
2021-09-20 22:38:03 +0200

    Downloader cleanup.

--- downloader.py
+++ downloader.py
@@ -8,9 +8,9 @@ class Downloader:
 
     def download(self, link, opts):
         with youtube_dl.YoutubeDL(opts) as ydl:
-            print("Download Started.")
+            #print("Download Started.")
             ydl.download([link])
-            print("Download Finished.")
+            #print("Download Finished.")
         return
 
     def add(self, t):
@@ -19,29 +19,3 @@ class Downloader:
             opts["outtmpl"] = i["file"]
             Process(target = self.download, args=(i["link"], opts)).start()
         print("End of add reached.")
-
-
-
-
-# async def downloader(self, search, filename):
-# print("Download Started.")
-# ydl_opts = {
-#     'outtmpl': filename,
-#     'format': 'bestaudio/best',
-#     'postprocessors': [{
-#         'key': 'FFmpegExtractAudio',
-#         'preferredcodec': 'wav',
-#         'preferredquality': self.bitRate
-#     }],
-#     'postprocessor_args': [
-#         '-ar', '16000'
-#     ],
-#     'ratelimit': int(os.getenv("ratelimit")),
-#     'prefer_ffmpeg': True,
-#     'keepvideo': False,
-#     'quiet': False
-# }
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     ydl.download([search])
-# print("Download Finished.")
-# return
